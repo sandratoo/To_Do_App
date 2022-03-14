10a886527e681e20bd35a5648a1220016b463c99
sandratoo <toosandraa@gmail.com>
2022-03-14 18:47:40 +0300

    unstyled todo app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,session
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -16,15 +16,30 @@ class Todo(db.Model):
 def index():
     #show items on the todo app
     todo_list = Todo.query.all()
-    print(todo_list)
-    return render_template("index.html")
+    return render_template("index.html", todo_list = todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title,complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     db.create_all()
-    
-    #create new item on the todo app
-    new_todo = Todo(title="Todo 1",complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-
     app.run(debug = True)
